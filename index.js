02734a2b3470279e5479e0f5623133a126404a42
Timothy Johnson <timothy.johnson@broadcom.com>
2021-05-20 11:22:59 -0400

    Implement update NPM deps script
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require("fs");
+const core = require("@actions/core");
+const { cosmiconfig } = require("cosmiconfig");
+const execa = require("execa");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const MAIN_BRANCHES = ["main", "master"];
+
+function getDependencies(branch, dev) {
+  const dependencies = dev ? branch.devDependencies : branch.dependencies;
+  if (!Array.isArray(dependencies)) {
+    return dependencies || {};
+  }
+
+  const dependencyMap = {};
+  for (const pkgName of dependencies) {
+    dependencyMap[pkgName] = branch.channel || (MAIN_BRANCHES.includes(branch.name) ? "latest" : branch.name);
+  }
+
+  return dependencyMap;
 }
+
+async function updateDependency(pkgName, pkgTag, dev) {
+  const packageJson = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+  const dependencies = packageJson[dev ? "devDependencies" : "dependencies"] || {};
+  const currentVersion = dependencies[pkgName];
+  const latestVersion = (await execa("npm", ["view", `${pkgName}@${pkgTag}`, "version"])).stdout;
+
+  if (currentVersion !== latestVersion) {
+    const npmArgs = dev ? ["--save-dev"] : ["--save-prod", "--save-exact"];
+    await execa("npm", ["install", `${pkgName}@${latestVersion}`, ...npmArgs]);
+  }
+}
+
+(async () => {
+  const { config } = await cosmiconfig("release").search();
+  const branchName = process.env.GITHUB_BASE_REF || process.env.GITHUB_REF.replace(/^refs\/heads\//, "");
+  const branch = config.branches.find((branch) => branch.name === branchName);
+
+  if (branch != null) {
+    const dependencies = getDependencies(branch, false);
+    const devDependencies = getDependencies(branch, true);
+
+    if (branch.dependencies) {
+      for (const [pkgName, pkgTag] of Object.entries(dependencies)) {
+        await updateDependency(pkgName, pkgTag, false);
+      }
+    }
+
+    if (branch.devDependencies) {
+      for (const [pkgName, pkgTag] of Object.entries(devDependencies)) {
+        await updateDependency(pkgName, pkgTag, true);
+      }
+    }
+
+    if (fs.existsSync("lerna.json") && (branch.dependencies || branch.devDependencies)) {
+      const dependencyList = [...Object.keys(dependencies), ...Object.keys(devDependencies)];
+      await execa("npx", ["-y", "--", "syncpack", "fix-mismatches", "--dev", "--prod", "--filter", dependencyList.join("|")]);
+      await execa("git", ["checkout", "package-lock.json"]);
+      await execa("npm", ["install"]);
+    }
+  } else {
+    core.info("Nothing to do since this is not a protected branch or a PR based on one");
+  }
+})().catch((error) => {
+  core.setFailed(error.message);
+});
